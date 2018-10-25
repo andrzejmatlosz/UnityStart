b3ea64dd002fd4d7c5f5cf0a33eecfe1e4db64c4
Andrzej Matłosz <andrzej.matlosz@gmail.com>
2018-10-25 11:28:10 +0200

    Lesson 3

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+    public GameObject ball;
+    public GameObject eyes;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButtonDown(0))
+        {
+            var newBall = Instantiate(ball, eyes.transform.position, eyes.transform.rotation);
+            var ballRigidBody = newBall.GetComponent<Rigidbody>();
+            ballRigidBody.AddForce(eyes.transform.forward * 500);
+            Destroy(newBall, 5); 
+        }
+	}
+}
