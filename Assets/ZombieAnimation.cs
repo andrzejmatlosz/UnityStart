9053f164fff1ee82474ca8f3f2758a1ca72ead46
Andrzej Matłosz <andrzej.matlosz@gmail.com>
2018-11-08 11:41:16 +0100

    zabawa z animacją

---
+++ Assets/ZombieAnimation.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieAnimation : MonoBehaviour {
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetBool("isWalking", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("jump");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            animator.SetTrigger("punch");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            animator.SetTrigger("kick");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.Rotate(0, -2f, 0);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.Rotate(0, 2f, 0);
+        }
+    }
+}
